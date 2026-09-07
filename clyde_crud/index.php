ae61171e4428a7f61bfe6bc39a7cf403f401ea8e
bakal ako! <clydepamintuan007@gmail.com>
2026-09-07 11:30:27 +0800

    Add files via upload

--- clyde_crud/index.php
+++ clyde_crud/index.php
@@ -1,46 +1,44 @@
 <?php
-    session_start();
-    include "config/database.php"
+session_start();
+include "config/database.php";
 
-    // if the user already logged in send them to dashboard
-   
-
-        {
-        if ($_SESSION["role"]== "admin"){
+// if the user is already login send them to dashboard
+if(isset($_SESSION["role"])){
+    if($_SESSION["role"]== "admin"){
         header("Location: admin/dashboard.php");
-        }
-
-        else{
-            header("Location: student/dashboard.php");
-        }
-        exit()
-        
-
-        }
-        $error = "";
-        if(isset($_POST["login"])){
-        //get data from login form.
-        $username = mysqli_escape_string($conn, $_POST["username"]);
-        $password = $_POST ["password"]
-        // find the user with username
-        $sql = "SELECT * FROM users
-        WHERE username = ' $username' LIMIT 1";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result));
-        // compare typed password with database 
-        if (password_verify($password, $user["password"])){
-            $_SESSION["user_id"] = $user ['id'];
-            $_SESSION["full_name"] = $user ['full_name'];
-            $_SESSION["role"] = $user ['role'];
-
-            if (user["role"]);
-        }
-
-        
     }
+    else{
+        header("Location: student/dashboard.php");
+    }
+    exit();
+}
+$error = "";
+if(isset($_POST["login"])){
 
+    $username = mysqli_escape_string($conn, $_POST["username"]);
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) == 1){
+        $user = mysqli_fetch_assoc($result);
+        if(password_verify($password, $user["password"])){
+            $_SESSION["user_id"] = $user['id'];
+            $_SESSION["full_name"] = $user['full_name'];
+            $_SESSION["role"] = $user['role'];
+
+            if($user["role" == "admin"]){
+                header("Location: admin/dashboard.php");
+            }
+            else{
+                header("Location: student/dashboard.php");
+            }
+            exit();
+
+        }
+    }
+}
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -55,13 +53,16 @@
         <div class="card"><div class="card-body p-4">
             <h2 class="text-center">Student Portal</h2>
             <p class="text-center text-muted">Admin and Student Login</p>
-            <?php if($error != "") ?>
-                <div class = "alert alert-danger><?php echo $error?></div>
-            <?php ?>
+            <?php  if($error != ""){?>
+            <div class="alert alert-danger"
+                    <?php echo $error;?>
+
+             <?php }?>
+
             <form method="POST">
                 <div class="mb-3"><label class="form-label">Username</label><input type="text" name="username" class="form-control"></div>
-                <div class="mb-3"><label class="form-label">Password</label><input type="password"name ="password" class="form-control"></div>
-                <button class="btn btn-primary w-100" type="submit" name="login">Login</button>
+                <div class="mb-3"><label class="form-label">Password</label><input type="password" name="password" class="form-control"></div>
+                <button class="btn btn-primary w-100"  type="submit" name="login">Login</button>
             </form>
         </div></div>
     </div>
